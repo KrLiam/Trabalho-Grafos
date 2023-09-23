151ff92e19b124c60b6eac3570c3491526a3d8a1
Mateus Goulart Chedid <63214688+MateusChedid@users.noreply.github.com>
2023-09-23 10:32:01 -0300

    Adiciona função ler arquivo

--- grafo.py
+++ grafo.py
@@ -5,16 +5,52 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Vertice:
   rotulo: str
-
+  index: int
 
 
 class Grafo:
     mapa_vizinhos: dict[Vertice, set[Vertice]]
     mapa_pesos: dict[frozenset[Vertice], float]
 
+
+
+
     @classmethod
-    def ler_arquivo(cls, nome: str):
-        ...
+    def ler_arquivo(self, file_name):
+
+        vertices = {}
+        with open(file_name, 'r') as arquivo:
+            ler_vertices = False
+            ler_arestas = False
+
+            for linha in arquivo:
+                linha = linha.strip()
+                
+                if linha.startswith('*vertices'):
+                    ler_vertices = True
+                    continue
+                
+                if ler_vertices:
+                    partes = linha.split()
+                    if len(partes) == 2:
+                        index, rotulo = partes
+                        vertice = Vertice(rotulo, index)
+                        vertices[index] = vertice
+                
+                if linha.startswith('*edges'):
+                    ler_arestas = True
+                    continue
+
+                if ler_arestas:
+                    partes = linha.split()
+                    if len(partes) == 3:
+                        u = vertices[partes[0]]
+                        v = vertices[partes[1]]
+                        w = float(partes[2])
+                        grafo.adicionar_aresta(u, v, w)
+        
+        return grafo
+
 
     def __init__(self):
         self.mapa_vizinhos = {}
@@ -52,4 +88,3 @@ class Grafo:
         if v not in self.mapa_vizinhos:
             self.mapa_vizinhos[v] = set()
         self.mapa_vizinhos[v].add(u)
-
