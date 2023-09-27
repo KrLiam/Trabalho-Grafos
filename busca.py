6119e02b863ae34790b57867f3211f8284621d49
Mateus Goulart Chedid <mateusgoulart25@gmail.com>
2023-09-26 21:09:32 -0300

    conserta busca em largura e adiciona print

--- busca.py
+++ busca.py
@@ -1,18 +1,39 @@
 from grafo import Grafo
 
 def buscaLargura (grafo: Grafo, s: int) -> tuple[dict[int, int], dict[int, int]]:
-    C = {v: False for v in grafo.vertices()}
+    C = set()
     D = {v: float("inf") for v in grafo.vertices()}
     A = {v: None for v in grafo.vertices()}
+
     D[s] = 0
-    Q = []
-    Q.append(s)
-    while len(Q) > 0:
+    C.add(s)
+    Q = [s]
+
+    while Q:
         u = Q.pop(0)
+
         for v in grafo.vizinhos(u):
-            if C[v] == False:
-                C[v] = True
+            if v not in C:
                 D[v] = D[u] + 1
                 A[v] = u
                 Q.append(v)
+                C.add(v)
+
     return D, A
+
+
+if __name__ == "__main__":
+    grafo = Grafo.ler_arquivo("entrada.txt")
+
+    D, A = buscaLargura(grafo, 1)
+    for v in grafo.vertices():
+        caminho = [v]
+
+        u = A[v]
+        while u is not None:
+            caminho.append(u)
+            u = A[u]
+            # print(u)
+
+        caminho_str = ",".join(str(i) for i in reversed(caminho))
+        print(f"{v}: {caminho_str}")
