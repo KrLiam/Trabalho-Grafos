260a1f042a338f45143242569904c4e67613aa62
William Kraus <88288206+KrLiam@users.noreply.github.com>
2023-11-28 21:24:08 -0300

    fizemos algo

--- Trabalho-3/Ford_Fulkerson.py
+++ Trabalho-3/Ford_Fulkerson.py
@@ -1,45 +1,45 @@
-from grafo import Grafo, ler_arquivo
+from grafo import GrafoDirigido, ler_arquivo
 from Edmonds_Karp import EdmondsKarp
 
 
-def FordFulkerson(grafo: Grafo, s: int, t: int, grafo_residual: Grafo) -> tuple[dict[int, int], dict[int, int]]:
-
+def FordFulkerson(
+    grafo: GrafoDirigido, s: int, t: int, grafo_residual: GrafoDirigido
+) -> tuple[dict[int, int], dict[int, int]]:
     F = 0
     while True:
-
-        #Encontrar um caminho que não fora analisado ainda
+        # Encontrar um caminho que não fora analisado ainda
         p = EdmondsKarp(grafo, s, t, grafo_residual)
         print(f"caminho aumentante: {p}")
         if p == 0:
             break
 
-        #Identificando a capacidade do caminho e adicionando-o ao fluxoi atual
+        # Identificando a capacidade do caminho e adicionando-o ao fluxoi atual
         arestas = []
-        for i in range(len(p)-1):
+        for i in range(len(p) - 1):
             u = p[i]
-            v = p[i+1]
-            arestas.append(grafo_residual.peso(u,v))
+            v = p[i + 1]
+            arestas.append(grafo_residual.peso(u, v))
 
         fp = min(arestas)
         F += fp
         print(f"fluxo aumentado em {fp}")
 
-        #Atualizando a capacidade residual
-        for i in range(len(p)-1):
+        # Atualizando a capacidade residual
+        for i in range(len(p) - 1):
             u = p[i]
-            v = p[i+1]
+            v = p[i + 1]
 
-            grafo_residual.alterar_peso_aresta(u,v,grafo_residual.peso(u,v) - fp)
-            grafo_residual.alterar_peso_aresta(v,u,grafo_residual.peso(v,u) + fp)
+            grafo_residual.alterar_peso_aresta(u, v, grafo_residual.peso(u, v) - fp)
+            grafo_residual.alterar_peso_aresta(v, u, grafo_residual.peso(v, u) + fp)
 
     return F
 
 
-def mostrar_grafo(grafo: Grafo):
+def mostrar_grafo(grafo: GrafoDirigido):
     import networkx as nx
     import matplotlib.pyplot as plt
 
-    ng = nx.Graph()
+    ng = nx.DiGraph()
     ng.add_nodes_from(grafo.vertices())
     ng.add_edges_from(grafo.arestas())
 
@@ -62,14 +62,11 @@ def mostrar_grafo(grafo: Grafo):
 
 
 if __name__ == "__main__":
-    grafo = ler_arquivo("entrada.txt")
-    mostrar_grafo(grafo)
+    grafo = ler_arquivo("wiki.net")
 
-    #Pedi pro chat gpt fazer essa funcao do grafo residual, entao nao sei se ta funcionando como devia
+    # Pedi pro chat gpt fazer essa funcao do grafo residual, entao nao sei se ta funcionando como devia
     grafo_residual = grafo.criar_grafo_residual()
 
-    mostrar_grafo(grafo_residual)
-
-    #Eu não sei como mandar pra função o ultimo vértice do grafo, o vertice t, por isso mandei 100 por enquanto 😁
-    A = FordFulkerson(grafo, 1, 6, grafo_residual)
+    # Eu não sei como mandar pra função o ultimo vértice do grafo, o vertice t, por isso mandei 100 por enquanto 😁
+    A = FordFulkerson(grafo, 1, 7, grafo_residual)
     print(A)
